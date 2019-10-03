2906de88098ab89d68709f3e4d58f6677913f886
ayushnagar123 <an431999@gmail.com>
2019-10-03 21:04:36 +0530

    Updates

---
+++ precache-manifest.2f192ffebac8c2ac839fea91a11585b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32df078fde98098012cfc1c72e13064",
+    "url": "/Smart-Reacr-List/index.html"
+  },
+  {
+    "revision": "64fc25f1889cc3e5d0da",
+    "url": "/Smart-Reacr-List/static/css/main.4ed9bdda.chunk.css"
+  },
+  {
+    "revision": "d11fc4876ce6d3177f81",
+    "url": "/Smart-Reacr-List/static/js/2.2886ec69.chunk.js"
+  },
+  {
+    "revision": "64fc25f1889cc3e5d0da",
+    "url": "/Smart-Reacr-List/static/js/main.644fe193.chunk.js"
+  },
+  {
+    "revision": "d7f4732fcdb99fd97632",
+    "url": "/Smart-Reacr-List/static/js/runtime-main.fb3523ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Smart-Reacr-List/static/media/logo.5d5d9eef.svg"
+  }
+]);
