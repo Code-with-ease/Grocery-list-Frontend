7508a4a8681aa3857d8b973eec68e1d56fdc044f
ayushnagar123 <an431999@gmail.com>
2019-10-07 03:00:03 +0530

    pwa

---
+++ build/precache-manifest.6bf307de10721002d7a720a0aa9892de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b629beb46a9f5f932b64217e99fde97e",
+    "url": "/Smart-List-React/index.html"
+  },
+  {
+    "revision": "e9d37d9f99e2ade8d102",
+    "url": "/Smart-List-React/static/css/main.4ed9bdda.chunk.css"
+  },
+  {
+    "revision": "d11fc4876ce6d3177f81",
+    "url": "/Smart-List-React/static/js/2.2886ec69.chunk.js"
+  },
+  {
+    "revision": "e9d37d9f99e2ade8d102",
+    "url": "/Smart-List-React/static/js/main.bf73cc83.chunk.js"
+  },
+  {
+    "revision": "7a07dad2dfcceea49d0d",
+    "url": "/Smart-List-React/static/js/runtime-main.d9aa55fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Smart-List-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
