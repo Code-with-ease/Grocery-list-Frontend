1b79ac913e4625a9b6c0818ef56c62b93ebefdd4
ayushnagar123 <an431999@gmail.com>
2019-10-26 01:43:07 +0530

    Updates

--- precache-manifest.f854220f39a32a663a006377872923b2.js
+++ precache-manifest.f854220f39a32a663a006377872923b2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60e7d7a68ea4c0d79d408eae46b85b23",
+    "revision": "ef2b58de6a11a9eb411faaf4c567f8fc",
     "url": "/Grocery-list-Frontend/index.html"
   },
   {
-    "revision": "d74dd8ff60deace414d4",
-    "url": "/Grocery-list-Frontend/static/css/main.4ed9bdda.chunk.css"
+    "revision": "e9194a283a30e51d5bb7",
+    "url": "/Grocery-list-Frontend/static/css/main.e8fcf2c1.chunk.css"
   },
   {
-    "revision": "69c1f0d7ac8f2b19eb52",
-    "url": "/Grocery-list-Frontend/static/js/2.64cda19c.chunk.js"
+    "revision": "eb28440670e255f00562",
+    "url": "/Grocery-list-Frontend/static/js/2.c9f893ff.chunk.js"
   },
   {
-    "revision": "d74dd8ff60deace414d4",
-    "url": "/Grocery-list-Frontend/static/js/main.2fce3266.chunk.js"
+    "revision": "e9194a283a30e51d5bb7",
+    "url": "/Grocery-list-Frontend/static/js/main.4be3b72a.chunk.js"
   },
   {
     "revision": "3ad013ef9ad59dc047e8",
