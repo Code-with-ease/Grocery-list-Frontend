31658946ded4b2deaf6961677af9d9e6d11fba7e
ayushnagar123 <an431999@gmail.com>
2019-11-22 19:48:08 +0530

    Updates

---
+++ precache-manifest.1a5457e02d2a1169e11a9934da6934c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e75583db7b64490443394842eb6387c",
+    "url": "/Grocery-list-Frontend/index.html"
+  },
+  {
+    "revision": "32f03f3e23349076c792",
+    "url": "/Grocery-list-Frontend/static/css/main.d719ee3d.chunk.css"
+  },
+  {
+    "revision": "07b0733af6e7b97b8af8",
+    "url": "/Grocery-list-Frontend/static/js/2.332c90e5.chunk.js"
+  },
+  {
+    "revision": "32f03f3e23349076c792",
+    "url": "/Grocery-list-Frontend/static/js/main.790c25d3.chunk.js"
+  },
+  {
+    "revision": "3ad013ef9ad59dc047e8",
+    "url": "/Grocery-list-Frontend/static/js/runtime-main.1d512a16.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Smart-List-React/precache-manifest.b14ff0e259c472baa04f535f9fa253a8.js"
+  "/Grocery-list-Frontend/precache-manifest.1a5457e02d2a1169e11a9934da6934c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Smart-List-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Grocery-list-Frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
